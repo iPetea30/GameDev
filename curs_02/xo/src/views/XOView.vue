db48a66336fec721ee7240927093f6710def59ec
iPetea30 <dumi.puia@gmail.com>
2023-07-11 20:45:53 +0300

    curs_03

--- curs_02/xo/src/views/XOView.vue
+++ curs_02/xo/src/views/XOView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import {reactive} from 'vue';
+// gabi.gutu@gmail.com
+import { reactive } from 'vue';
 
-let tabla = reactive ([
-    ['', '', 'X',],
-    ['', '0', '',],
+let tabla = reactive([
+    ['', '', '',],
+    ['', '', '',],
     ['', '', '',],
 ]);
 const valori = ['X', '0'];
@@ -11,44 +12,51 @@ let gameOver = false;
 let indexMutare = 0;
 
 
-function equalsRow (indexRow) {
+function equalsRow(indexRow) {
     const linie = tabla[indexRow];
     const ref = linie[0];
+    if (ref === '') return;
     for (let i = 1; i < linie.length; i++) {
         if (ref !== linie[i]) return false;
     }
     return true;
 }
 
-function equalsCol (indexCol) {
+function equalsCol(indexCol) {
     const ref = tabla[0][indexCol];
+    if (ref === '') return;
     for (let i = 1; i < tabla.length; i++) {
         if (ref !== tabla[i][indexCol]) return false;
     }
     return true;
 }
-function principala (){
-    const ref = tabla [0][0];
-    for ( let i = 1; i < tabla.length; i++) {
+function principala() {
+    const ref = tabla[0][0];
+    if (ref === '') return;
+    for (let i = 1; i < tabla.length; i++) {
         if (ref !== tabla[i][i]) return false;
     }
     return true;
 }
 
-function secundara () {
-    const ref = tabla [0][tabla.length];
+function secundara() {
+    const ref = tabla[0][tabla.length - 1];
+    if (ref === '') return;
     for (let i = 1; i < tabla.length; i++) {
         if (ref !== tabla[i][tabla.length - i - 1]) return false;
     }
     return true;
 }
 
-function check () {
-    for (let i = 0; i < tabla.length; i++)
-    equalsRow(i);
+// function check () {
+//     for (let i = 0; i < tabla.length; i++)
+//     equalsRow(i);
+// }
+function endGame() {
+    alert(valori[indexMutare] + ' a castigat !');
+    gameOver = true;
 }
-
-function clickCell(row, col) {
+function clickCell(row, col, cellId) {
     if (gameOver) return;
     console.log('Clicked cell [' + row + '][' + col + ']'); //[1][2]
     if (tabla[row][col] !== '') {
@@ -56,9 +64,19 @@ function clickCell(row, col) {
         retun;
     }
     tabla[row][col] = valori[indexMutare];
-    if (equalsRow(row) || equalsCol(col)){
-        alert (valori[indexMutare] + ' a castigat !');
-        gameOver = true;
+    const cellElement = document.getElementById(cellId);
+    cellElement.classList.add('animate__animated', 'animate__heartBeat');
+    anime({
+        targets: '#' + cellId,
+        translateX: 50
+    });
+    if (
+        equalsRow(row) ||
+        equalsCol(col) ||
+        (row === col && principala()) ||
+        (col === tabla.length - row - 1 && secundara())
+    ) {
+        endGame();
     }
     // if(equalsCol(col)) {
     //     alert(valori[indexMutare] + ' a castigat!');
@@ -77,7 +95,9 @@ function clickCell(row, col) {
 
             <tbody>
                 <tr v-for="(row, indexRow) in tabla" v-bind:key="indexRow">
-                    <td v-for="(cell, indexCol) in row" class="cell" @click="clickCell(indexRow, indexCol)">
+                    <!-- cell -1-2, cell-2-2 -->
+                    <td v-bind:id="'cell-' + indexRow + '-' + indexCol" v-for="(cell, indexCol) in row" class="cell"
+                        @click="clickCell(indexRow, indexCol, 'cell-' + indexRow + '-' + indexCol)" v-bind:key="indexCol">
                         <span v-if="cell != ''">{{ cell }}</span>
                         <span v-if="cell == ''">_</span>
                         <!-- <span v-else>_</span> -->
